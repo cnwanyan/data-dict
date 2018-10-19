2503a33e574e026b3ef67a3be8df09c627698271
jxwyxccs <jxwyxccs@163.com>
2018-10-19 17:13:30 +0800

    完成了基础的生成功能

--- src/main/java/com/chengs/dict/Application.java
+++ src/main/java/com/chengs/dict/Application.java
@@ -13,20 +13,19 @@ public class Application{
 	
     public static void main(String[] args) throws Exception{
     	CreaterService cs=new CreaterService();
-    	Scanner scan = new Scanner(System.in);
-    	String url,user,password,dbName;
-    	System.out.println("请输入数据库连接URL:");
-    	url = scan.nextLine();
+    	/*System.out.println("请输入数据库连接URL:");
+    	String url = scan.nextLine();
     	System.out.println("请输入数据库用户名:");
-    	user = scan.nextLine();
+    	String user = scan.nextLine();
     	System.out.println("请输入数据库密码:");
-    	password = scan.nextLine();
+    	String password = scan.nextLine();
     	System.out.println("请输入需要输出字典的数据库名称:");
-    	dbName = scan.nextLine();
-//    	cs.creater("jdbc:mysql://127.0.0.1:3306/iisp?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&useSSL=false",
-//    			"root", "root", "iisp");
-    	scan.close();
-    	cs.creater(url, user, password, dbName);
+    	String dbName = scan.nextLine();*/
+    	if(args.length != 4) {
+    		throw new Exception("参数错误");
+    	}else {
+    		cs.creater(args[0], args[1], args[2], args[3]);
+    	}
     }
     
 }
