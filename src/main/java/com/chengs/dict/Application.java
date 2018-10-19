497cd3839660c69ad06537434f6b47dae372222f
jxwyxccs <jxwyxccs@163.com>
2018-10-19 14:53:07 +0800

    新增主启动类

---
+++ src/main/java/com/chengs/dict/Application.java
@@ -0,0 +1,14 @@
+package com.chengs.dict;
+
+/**
+ *      主启动类
+ * @author Shu Cheng
+ *
+ */
+public class Application{
+	
+    public static void main(String[] args){
+        System.out.println( "Hello World!" );
+    }
+    
+}
